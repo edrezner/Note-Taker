cfbd6874b8e3e3c8d8d46db1ad09910fd71b59bc
Seven <edrezner7@gmail.com>
2023-04-25 18:04:39 -0400

    finished adding delete route and also commented server.js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Set requires
 const express = require("express");
 const path = require("path");
 const PORT = 3001;
@@ -11,10 +12,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Sets first get route to display notes.html file
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+// Creates get route for displaying notes in database db.json file
 app.get("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "db", "db.json"), (err, data) => {
     if (err) {
@@ -29,6 +32,7 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
+// Post route for saving a new note. Uses uuid package to attach a unique id
 app.post("/api/notes", (req, res) => {
   const newNote = {
     id: uuidv4(),
@@ -36,14 +40,17 @@ app.post("/api/notes", (req, res) => {
     text: req.body.text,
   };
 
+  // db.json file must be read and parsed first since fs does not automatically parse or stringify json strings/javascript objects
   fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ message: "Error saving note" });
     } else {
+      // pushes note to be saved to the array of objects
       const notes = JSON.parse(data);
       notes.push(newNote);
 
+      // updates the json file
       fs.writeFile(
         path.join(__dirname, "db", "db.json"),
         JSON.stringify(notes),
@@ -60,11 +67,10 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
-app.delete("api/notes/:id", (req, res) => {
+// Creates a delete route
+app.delete("/api/notes/:id", (req, res) => {
   // store id in a variable
-  // read contents of the file with fs and then JSON parse
-  // use findIndex() to find the matching id with the variable
-  // use splice method to remove that index
+
   // write the file back to the json file using stringify and writeFile
   const id = req.body.id;
 
@@ -74,9 +80,14 @@ app.delete("api/notes/:id", (req, res) => {
       res.status(500).json({ message: "Error deleting note" });
     } else {
       const deleteNote = JSON.parse(data);
-      const index = deleteNote.findIndex((data) => data.id === id);
+
+      // use findIndex() to find the matching id with the variable
+      const index = deleteNote.findIndex((note) => note.id === id);
+
+      // use splice method to remove that index
       deleteNote.splice(index, 1);
 
+      // updates contents of the file with fs
       fs.writeFile(
         path.join(__dirname, "./db/db.json"),
         JSON.stringify(deleteNote),
@@ -93,6 +104,7 @@ app.delete("api/notes/:id", (req, res) => {
   });
 });
 
+// Sets wildcard route
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
