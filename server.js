1a1c7700fe3e676422611fe8e90384f86bcdac5c
Seven <edrezner7@gmail.com>
2023-04-25 14:48:42 -0400

    Added a write file block for the post (save) route. Added uuid library for unique ids. Moved the wildcard get route to bottom of the page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const dbData = require("./db/db.json");
 const PORT = 3001;
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -14,14 +15,57 @@ app.use(express.static("public"));
 let noteData = [];
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: "Could not retrieve notes" });
+    } else {
+      console.log(data);
+      const notes = JSON.parse(data);
+      res.json(notes);
+      console.log(notes);
+    }
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: "Error saving note" });
+    } else {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+
+      fs.writeFile(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ message: "Error saving note" });
+          } else {
+            res.json(newNote);
+          }
+        }
+      );
+    }
+  });
 });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-app.get("/api/notes", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
